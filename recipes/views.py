224290a76738dc6cb62166334be0ed00297b5079
Afonso Miqueias <afonsogroove@gmail.com>
2024-02-07 16:53:46 +0100

    tamplates

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request): 
-  return HttpResponse('HOME AFONSO')
+  return render(request, 'recipes/home.html')
 
 
 def contato(request): 
